fd439f033cd899df3ec2c20af9455e8c60cddf73
PeoneEr <peoneerko@yandex.ru>
2015-09-17 23:04:51 +0600

    add links for new pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'main_page#index', :defaults => {:prefix => 'bicycle'}
+  root to: 'main_page#index', :defaults => { :prefix => 'bicycle' }
   resources :phone_orders, only: [:create]
-  get 'mopedi_scooteri', :as => 'motorbike', :to => "main_page#index", :defaults => {:prefix => 'motorbike'}
+  get 'mopedi_scooteri', :as => 'motorbike',    :to => 'main_page#index', :defaults => { :prefix => 'motorbike' }
+  get 'avto_mashini',    :as => 'avto_mashini', :to => 'main_page#index', :defaults => { :prefix => 'avto_mashini' }
+  get 'motocykli',       :as => 'motocykli',    :to => 'main_page#index', :defaults => { :prefix => 'motocykli' }
 
   namespace :manage do
     root to: 'page_meta#index'
