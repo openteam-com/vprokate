08817f854c0a3da06256272c16b9c703bf09c016
PeoneEr <peoneerko@yandex.ru>
2016-06-06 14:24:14 +0700

    some improves

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   root to: 'main_page#index', :defaults => { :prefix => 'bicycle' }
   resources :phone_orders, only: [:create]
-  get 'mopedi_scooteri', :as => 'motorbike',    :to => 'main_page#index', :defaults => { :prefix => 'motorbike' }
-  get 'avto_mashini',    :as => 'avto_mashini', :to => 'main_page#index', :defaults => { :prefix => 'avto_mashini' }
-  get 'motocykli',       :as => 'motocykli',    :to => 'main_page#index', :defaults => { :prefix => 'motocykli' }
+  get 'mopedi_scooteri',             :as => 'motorbike',                :to => 'main_page#index', :defaults => { :prefix => 'motorbike' }
+  get 'avto_mashini',                :as => 'avto_mashini',             :to => 'main_page#index', :defaults => { :prefix => 'avto_mashini' }
+  get 'motocykli',                   :as => 'motocykli',                :to => 'main_page#index', :defaults => { :prefix => 'motocykli' }
+  get 'kuplu_moped_velosiped',       :as => 'kuplu_moped_velosiped',    :to => 'main_page#index', :defaults => { :prefix => 'kuplu_moped_velosiped' }
 
   namespace :manage do
     root to: 'page_meta#index'
+
     resources :page_meta, only: [:update, :edit]
     resources :phone_orders, only: [:index]
   end
